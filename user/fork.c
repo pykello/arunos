9dbb5f9b41a6ac14f79b6f4e1db4d83d64d71640
Hadi Moshayedi <hadi@moshayedi.net>
2014-08-21 23:20:20 +0300

    Print fork() return value at fork test program.

--- user/fork.c
+++ user/fork.c
@@ -4,11 +4,11 @@
 void _start()
 {
 	int pid = 0;
+	int fork_result = 0;
 
-	fork();
-
+	fork_result = fork();
 	pid = getpid();
-	printf("hello from pid: %d\n", pid);
+	printf("hello from pid: %d, fork returned: %d\n", pid, fork_result);
 
 	exit(0);
 }
